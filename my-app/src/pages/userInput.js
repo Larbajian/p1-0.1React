173d9b7cfb6b1c73b15e154f08bf8e7988886327
unknown <lexiarbajianclt@gmail.com>
2023-05-13 08:45:43 -0400

    userInput component (no styling)

---
+++ my-app/src/pages/userInput.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+const userInput = () => {
+  const [email, setEmail] = useState({ email: "" });
+  const [files, setSelectedFiles] = useState([]);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleFileChange = (event) => {
+    setSelectedFiles([...event.target.files]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (files.length > 0) {
+
+        const formData = new FormData();
+        formData.append("email", email);
+        for (let i = 0; i < files.length; i++) {
+            formData.append("files", files[i]);
+        }
+
+        fetch("http://localhost:8000/upload", {
+            method: "POST",
+            body: formData,
+        })
+            .then((response) => response.json())
+            .then((result) => {
+            console.log("Success:", result);
+            })
+            .catch((error) => {
+            console.error("Error:", error);
+            });
+    }
+    
+  };
+
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <form onSubmit={handleSubmit}>
+ 
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+          <input
+          type="file"
+          placeholder="Upload File"
+          accept=".pdf, .doc, .docx, .txt, .mp4"
+          onChange={handleFileChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default userInput;
